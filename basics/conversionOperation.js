4f0241f986d64d9d66f32256ae970992d455748d
Arpita-ai-hub <nilakshi2522005@gmail.com>
2026-08-15 19:34:37 +0000

    Add opertions

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -1,30 +1,64 @@
-let score = "12abc"
-//let score = true
-//let score = null
-// let score = undefined
-console.log(typeof score)
-console.log(typeof(score))
+// let score = "12abc"
+// //let score = true
+// //let score = null
+// // let score = undefined
+// console.log(typeof score)
+// console.log(typeof(score))
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
-/*
-"33" => 33
-"33abc" => NaN
-true => 1; false => 0
+// /*
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
 
-*/
+// */
 
-let isLoggedIn = 1
-let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn)
+// let isLoggedIn = 1
+// let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
 
-// 1 => true; 0 => false
-// "" => false
-// "nilakshi" => true
+// // 1 => true; 0 => false
+// // "" => false
+// // "nilakshi" => true
 
-let anyNumber = 21
-let stringNumber = String(anyNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let anyNumber = 21
+// let stringNumber = String(anyNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//******************************Operations********************************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log(2+2)
+console.log(2*2)
+console.log(2**2)
+console.log(2**3)
+console.log(2/3)
+console.log(2%3)
+
+let str1 = "Arpita "
+let str2 = "Tiwari"
+let str3 = str1 + str2
+console.log(str3);
+console.log("1"+2)
+console.log(1+"2")
+
+console.log("1"+ 2 + 2)
+console.log(1 + 2 +"2")
+console.log((3+4) * 5 % 3);
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+// From mdn study inrement and decrement -> prefix postfix
+// link to study
+//
