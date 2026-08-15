8a084e5a4840f4ff3b9b83cafe8af772e256d984
Arpita-ai-hub <nilakshi2522005@gmail.com>
2026-08-15 17:32:54 +0000

    Conversion of datatypes

---
+++ basics/conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "12abc"
+//let score = true
+//let score = null
+// let score = undefined
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+/*
+"33" => 33
+"33abc" => NaN
+true => 1; false => 0
+
+*/
+
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "nilakshi" => true
+
+let anyNumber = 21
+let stringNumber = String(anyNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
